1fe2d339c02085b2aa7754da3ab4cbef0e7d1492
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-16 15:52:44 -0700

    MongoDB get and post
    using postman

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -1,18 +1,50 @@
 // importing 
 import express from 'express';
+import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
 
 // app config 
 const app = express();
 const port = process.env.PORT || 9000;
 
 // middleware 
+app.use(express.json());
 
 // DB config 
+const connection_url = 'mongodb+srv://admin:PsIs5GcL5sT7whA5@cluster0.7x7w3.mongodb.net/whatsappdb?retryWrites=true&w=majority';
+mongoose.connect(connection_url, {
+    useCreateIndex: true,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+    // helps mongoose to work smoothly
+});
 
 // ????
 
 // api routes
 app.get('/',(req, res)=>res.status(200).send('hello world'));
+
+app.get('/messages/sync', (req, res) => {
+    Messages.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body
+
+    Messages.create(dbMessage, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
 
 // listen
 app.listen(port, ()=>console.log(`Listening on localhost:${port}`));
